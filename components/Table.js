c7d91826c5570c438d563d6d38dcb97ee4fc1a35
Michael Sikora <michaelsikora@Michaels-MacBook-Pro.local>
2020-09-06 21:22:35 -0500

    added changed to Table Compo

--- components/Table.js
+++ components/Table.js
@@ -12,11 +12,30 @@ const Table = () => {
 
         Feel free to delegate to as many components as you like or make Table a class-based components.
     */
+   const [state, setState] = useState([])
+    useEffect(() => {
+        fetch(URL).then(
+            res => {return res.json()}
+        ).then(data => {
+            setState(data.foods)
+        })
+    })
 
     return (
         <section>
             <table>
-                {/* Table Contents */}
+                <tr>
+                    <th>Calories</th>
+                    <th>Carbs</th>
+                    <th>Name</th>
+                    <th>Quantity</th>
+                </tr>
+                {state.map(food => <tr>
+                    <td>food.calores</td>
+                    <td>food.carbohydrates</td>
+                    <td>food.name</td>
+                    <td>food.quantity</td>
+                </tr>)}
             </table>
         </section>
     )
